afdfbf9c717fe80d15980547735a719855296c4e
arai riko <rikoriko3021@icloud.com>
2024-11-11 15:01:05 +0900

    一旦保存

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COACHTECHフリマ</title>
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="">
+    @yield('css')
+</head>
+
+<body>
+    <header>
+        <div class="coachtech_flea_market">
+            <img class="coachtech_logo" src="/img/logo.svg">
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
---
+++ src/resources/views/layouts/auth.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COACHTECHフリマ</title>
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="">
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <div class="header_layout">
+            <div class="coachtech_flea_market">
+                <a class="coachtech_flea_market_title" href="/">
+                    <img class="coachtech_flea_market_logo" src="/img/logo.svg">
+                </a>
+            </div>
+            <div class="search_box">
+                <input type="search" name="search_space" placeholder="なにをお探しですか？">
+            </div>
+            <div class="nav">
+                <a href="/logout">ログアウト</a>
+                <a href="/mypage">マイページ</a>
+                <a href="/sell">出品</a>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
